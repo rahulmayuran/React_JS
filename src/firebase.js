d5f52ba4de0840f703efa08031c3e6ba1ebb3fec
rahulmayuran <rahulmayuran1996@gmail.com>
2022-09-24 17:58:29 +0530

    Added dependencies and temp config file

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyBD8OnRIaNxiKufhCOn0ULFJ1KsLC0j7qg",
+    authDomain: "clone-1cd51.firebaseapp.com",
+    projectId: "clone-1cd51",
+    storageBucket: "clone-1cd51.appspot.com",
+    messagingSenderId: "246857592841",
+    appId: "1:246857592841:web:df1323cd0df9828b2788d8",
+    measurementId: "G-744JWV9FWH"
+  };
