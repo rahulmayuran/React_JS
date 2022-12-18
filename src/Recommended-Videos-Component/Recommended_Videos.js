41496ec90234269ce696d7aa4bc0fe2d5fa043a1
rahulmayuran <rahulmayuran1996@gmail.com>
2022-12-18 23:50:01 +0530

    Added Links to the images

--- src/Recommended-Videos-Component/Recommended_Videos.js
+++ src/Recommended-Videos-Component/Recommended_Videos.js
@@ -13,6 +13,7 @@ function Recommended_Videos() {
           channelImage= "https://yt3.ggpht.com/BF-_x67tLWk6uvsJLnhrtRMps0LZeCuPLVKCYXwdqY8dqWby9liaKd3lcQozs3JA2jqRXIkJzQ=s88-c-k-c0x00ffffff-no-rj"
           channel= "Khanvict"
           image= "https://i.ytimg.com/vi/DrKLYvLPidw/hq720.jpg?sqp=-oaymwEcCNAFEJQDSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLC5wF1Ew3jg8sQqtW2TZ71tBTwzYQ"
+          embedId = "DrKLYvLPidw"
         />
         <VideoCard
           title = "Nanotechnology: A New Frontier"
@@ -21,6 +22,7 @@ function Recommended_Videos() {
           channelImage= "https://yt3.ggpht.com/ytc/AMLnZu_-XxX1PRjLfpIQ0rsxy-x53hpfy-JTOH81iG1ufw=s88-c-k-c0x00ffffff-no-rj"
           channel= "Aperture"
           image = "https://i.ytimg.com/vi/Mr7IEvlflnI/hqdefault_252266.jpg?sqp=-oaymwEcCNACELwBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLD7xkWrtWyLhj4BpIPamEP9X7nv-w"
+          embedId = "OLa8DQkKlyU"
         />
         <VideoCard 
           title = "Deep Koliis - Stand (OAC)"
@@ -29,6 +31,7 @@ function Recommended_Videos() {
           channelImage= "https://yt3.ggpht.com/SgOkUbgJdeZip2YlZ28S6MUOG8QKoUGNzEsF0WafmXlcDNLpftuSr78t_PU6oNyzpENHnBMt=s88-c-k-c0x00ffffff-no-nd-rj"
           channel= "Deep Koliis"
           image= "https://i.ytimg.com/vi/6YV7kKhAQz0/hqdefault.jpg?sqp=-oaymwE2CNACELwBSFXyq4qpAygIARUAAIhCGAFwAcABBvABAfgB_gmAAtAFigIMCAAQARhlIGUoZTAP&rs=AOn4CLBDdUKDzwdZVDnNhq9_5I-lC3WXrg"
+          embedId = "6YV7kKhAQz0"
         />
         <VideoCard 
           title = "Deep Koliis - project 41 (OAC)"
@@ -37,7 +40,8 @@ function Recommended_Videos() {
           channelImage= "https://yt3.ggpht.com/SgOkUbgJdeZip2YlZ28S6MUOG8QKoUGNzEsF0WafmXlcDNLpftuSr78t_PU6oNyzpENHnBMt=s88-c-k-c0x00ffffff-no-nd-rj"
           channel= "Deep Koliis"
           image = "https://i.ytimg.com/vi/P6RlrA5AoDc/hqdefault.jpg?sqp=-oaymwE2CNACELwBSFXyq4qpAygIARUAAIhCGAFwAcABBvABAfgB_gmAAtAFigIMCAAQARhlIGUoZTAP&rs=AOn4CLAyjVbfEWsQGc-2Jv9GpwgCOAfGHA"
-        />
+          embedId = "P6RlrA5AoDc"
+          />
         <VideoCard 
           title = "Deep Koliis - You're My Home(OAC)"
           views = "11k views"
@@ -45,7 +49,8 @@ function Recommended_Videos() {
           channelImage= "https://yt3.ggpht.com/SgOkUbgJdeZip2YlZ28S6MUOG8QKoUGNzEsF0WafmXlcDNLpftuSr78t_PU6oNyzpENHnBMt=s88-c-k-c0x00ffffff-no-nd-rj"
           channel= "Deep Koliis"
           image = "https://i.ytimg.com/vi/NPZ8lnh8X_Q/hqdefault.jpg?sqp=-oaymwE2CNACELwBSFXyq4qpAygIARUAAIhCGAFwAcABBvABAfgB_gmAAtAFigIMCAAQARhlIGUoZTAP&rs=AOn4CLC0CfabMsxCz2OiNd5Lojjm_xPgOw"
-        />
+          embedId = "NPZ8lnh8X_Q"
+          />
         <VideoCard
           title = "17 Coolest Gadgets You Can Actually Buy"
           views = "1.7M views"
@@ -53,7 +58,8 @@ function Recommended_Videos() {
           channelImage= "https://yt3.ggpht.com/ytc/AMLnZu8DYCKjfSoS0ZGdCqNy80XKp45drew_5ALGJabzyg=s68-c-k-c0x00ffffff-no-rj"
           channel= "YouFact Tech"
           image = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSujb4nGtWhY9L1MdAdKemPWRyB_mAMBPm_fw&usqp=CAU"
-        />
+          embedId = "qiMnSaZWf3M"
+          />
         <VideoCard 
           title = "Deep Koliis - Universe (OAC)"
           views = "2k views"
@@ -61,7 +67,8 @@ function Recommended_Videos() {
           channelImage= "https://yt3.ggpht.com/SgOkUbgJdeZip2YlZ28S6MUOG8QKoUGNzEsF0WafmXlcDNLpftuSr78t_PU6oNyzpENHnBMt=s88-c-k-c0x00ffffff-no-nd-rj"
           channel= "Deep Koliis"
           image= "https://i.ytimg.com/vi/9qgtu0poYtQ/hqdefault.jpg?sqp=-oaymwE2CNACELwBSFXyq4qpAygIARUAAIhCGAFwAcABBvABAfgB_gmAAtAFigIMCAAQARhlIGUoZTAP&rs=AOn4CLCG1rqxrcOqICZhJTpF_P3jwPpL7w"
-        />
+          embedId = "9qgtu0poYtQ"
+          />
       </div>
     </div>
   )
--- src/Recommended-Videos-Component/VideoCard/VideoCard.js
+++ src/Recommended-Videos-Component/VideoCard/VideoCard.js
@@ -2,29 +2,31 @@ import React from 'react'
 import './VideoCard.css';
 import Avatar from "@mui/material/Avatar"
 
-function VideoCard({ image, title, channel, views, timestamp, channelImage }) {
+function VideoCard({ image, title, channel, views, timestamp, channelImage, embedId }) {
+
 
   return (
-    <div className='videoCard'>
-      <img className='videoCard__thumbnail' 
-        src={image} 
-        alt="" 
+  <div className='videoCard'>
+    <img className='videoCard__thumbnail'
+      src={image}
+      alt="" 
+      onClick={()=> window.open(`https://www.youtube.com/watch?v=${embedId}`, "_blank")}
+    />
+    <div id='videoCard__info'>
+      <Avatar
+        className="videoCard__avatar"
+        alt={channel}
+        src={channelImage}
       />
-      <div id='videoCard__info'>
-        <Avatar 
-          className="videoCard__avatar" 
-          alt={channel}
-          src={channelImage} 
-        />
-        <div className='videoCard__text'>
-          <h5>{title}</h5>
-          <p>{channel}</p>
-          <p>
-            {views} • {timestamp}
-          </p>
-        </div>
+      <div className='videoCard__text'>
+        <h5>{title}</h5>
+        <p>{channel}</p>
+        <p>
+          {views} • {timestamp}
+        </p>
       </div>
     </div>
+  </div>
   )
 }
 
