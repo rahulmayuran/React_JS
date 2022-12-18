23f8c9b534ae5d8bc19f60f56d670b1c349348ef
rahulmayuran <rahulmayuran1996@gmail.com>
2022-12-19 00:06:24 +0530

    Fixed the search page

--- src/Search-Page-Component/VideoRow/VideoRow.js
+++ src/Search-Page-Component/VideoRow/VideoRow.js
@@ -1,22 +1,19 @@
 import React from 'react';
 import './VideoRow.css';
 
-function VideoRow({
-    views,
-    subs,
-    desc,
-    timestamp,
-    channel,
-    title,
-    image
-}) {
+function VideoRow({ views, subs, desc, timestamp, channel, title, image, embedId}) 
+{
     return (
         <div className='videoRow'>
-            <img src={image} alt="" />
+            <img
+                src={image}
+                alt=""
+                onClick={() => window.open(`https://www.youtube.com/watch?v=${embedId}`, "_blank")}
+            />
             <div className='videoRow__text'>
                 <h3>{title}</h3>
                 <p className='videoRow__headline'>
-                    {channel} • {subs} Subscribers {views} views • {timestamp}
+                    {channel} • {subs} Subscribers • {views} views • {timestamp}
                 </p>
                 <p className='videoRow__desc'>{desc}</p>
             </div>
